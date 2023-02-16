8caa698d70449b26d46f7af380f7ae808f3b884c
aaronlee <aaronlee.uva@gmail.com>
2023-02-15 22:30:54 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import requests
+import json
+
+braze_track = "https://rest.iad-06.braze.com/users/track"
+
+track_headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer 8d2ed3c7-cb38-4a67-9001-9051f4a8a02e"
+}
+
+df = pd.read_csv("easterseals_attribute.csv")
+# print(df.values)
+
+all_data = [x[0] for x in df.values]
+# print(all_data)
+
+n = 0
+for x in all_data[::]:
+    n += 1
+    track = {
+        "attributes":
+            [{
+                "external_id": x,
+                "initial_source": "easterseals"
+            }]
+    }
+
+    track_user = json.dumps(track)
+    # print(f"Row {n}:", track_user)
+
+    # response = requests.post(url=braze_track, data=track_user, headers=track_headers)
+    # print(f"Row {n}: {x} Response: ", response.json())
+
